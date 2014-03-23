59c87cfc6b029ffd3d0ba564f3eef94ae03b7926
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-03-23 11:39:30 +0900

    make oauth.rb

--- delete_twitter_his.rb
+++ delete_twitter_his.rb
@@ -1,31 +1,8 @@
 #coding: utf-8
 require 'twitter'
 require 'csv'
-require 'oauth'
-require 'oauth/consumer'
-require './keys.rb'
-
-SourcePath = File.expand_path('../', __FILE__)
-TokenFile = "#{SourcePath}/token"
-
-def oauth_first
-  @consumer = OAuth::Consumer.new(CONSUMER_KEY ,CONSUMER_SECRET,{
-      :site=>"https://api.twitter.com"
-	    })
-
-  @request_token = @consumer.get_request_token
-
-  puts "Please access this URL: #{@request_token.authorize_url}"
-  puts "and get the Pin code."
-
-  print "Enter your Pin code:"
-  pin  = gets.chomp
-
-  @access_token = @request_token.get_access_token(:oauth_verifier => pin)
-
-  open(TokenFile, "a" ){|f| f.write("#{@access_token.token}\n")}
-  open(TokenFile, "a" ){|f| f.write("#{@access_token.secret}\n")}
-end
+require './src/keys.rb'
+require './src/oauth.rb'
 
 unless File::exist?(TokenFile)
     oauth_first
